16358bd2abcfdf328021385eae5c161069e5be9b
jose luis <josloe@gmail.com>
2020-09-29 14:15:14 +0200

    template vercel

--- index.py
+++ index.py
@@ -1,17 +1,35 @@
-# from sanic import Sanic
-# from sanic.response import json
-# app = Sanic()
-
-
-# @app.route('/')
-# @app.route('/<path:path>')
-# async def index(request, path=""):
-#     return json({'hello': path})
 
 from flask import Flask, Response
+import os
+# configuracion de puertos, path, etc...
+# import settings
+
+# from datetime import datetime, timedelta
+# import calendar
+
+from flask import Flask
+from flask_cors import CORS
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+from flask import session
+from flask.json import jsonify
+
+
+
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+@app.route("/", methods=["get"])
+def home():
+    return Response("Perct")
+
+@app.route("/api", methods=["get"])
+def home():
+    return jsonify({"resultado": "ok"})
+
+
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def catch_all(path):
+#     return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
