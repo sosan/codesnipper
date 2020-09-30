53370e77be1d67225e98f61c4b284ce7d6b956f6
jose luis <josloe@gmail.com>
2020-09-30 07:17:04 +0200

    fixed

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-
 from flask import Flask, Response
 import os
-# configuracion de puertos, path, etc...
 import settings
 settings.readconfig()
-
-# from datetime import datetime, timedelta
-# import calendar
 
 from flask import Flask
 from flask_cors import CORS
@@ -26,9 +21,6 @@ from ModuloLogica.Logica import ManagerLogica
 managerlogica = ManagerLogica()
 
 app = Flask(__name__)
-# CORS(app,  supports_credentials=True, resources={r"/api/*": {"origins": "*"}})
-#  resources={r"/api": {"origins": "*"}}
-# CORS(app)
 
 @app.route("/", methods=["get"])
 def home():
@@ -40,43 +32,42 @@ def api_get():
     return redirect(url_for("home"))
 
 
-# @cross_origin(origin="localhost", headers=["Content- Type","Authorization"])
-@app.route("/api", methods=["post"])
-def api_post():
+# @app.route("/api", methods=["post"])
+# def api_post():
 
-    if ("apiToken" in request.form and "idPage" in request.form):
-        if request.form["apiToken"] == "" or request.form["apiToken"] == "":
-            return jsonify({"resultado": "error"})
+#     if ("apiToken" in request.form and "idPage" in request.form):
+#         if request.form["apiToken"] == "" or request.form["apiToken"] == "":
+#             return jsonify({"resultado": "error"})
 
-        uuid = managerlogica.getUUIdPage(request.form["idPage"])
-        if uuid == None:
-            return jsonify({"resultado": "error"})
+#         uuid = managerlogica.getUUIdPage(request.form["idPage"])
+#         if uuid == None:
+#             return jsonify({"resultado": "error"})
 
-        client = NotionClient(request.form["apiToken"])
+#         client = NotionClient(request.form["apiToken"])
 
-        # uuid
-        # page = client.get_block(uuid)
-        # token = "e90da1e31347b2b61836546b0af4378b6076d67a72a82804996450eb3ece399a12bc39fd89a81dd0592593ed556043b0cbf6dcea765da4416ebeaec81e43141f7aa6d25d2423cb7f163e109da8f2"
-        # url
+#         # uuid
+#         # page = client.get_block(uuid)
+#         # token = "e90da1e31347b2b61836546b0af4378b6076d67a72a82804996450eb3ece399a12bc39fd89a81dd0592593ed556043b0cbf6dcea765da4416ebeaec81e43141f7aa6d25d2423cb7f163e109da8f2"
+#         # url
 
 
         
 
-        url = "https://www.notion.so/cecf1c9de960437c80f4f3b9940a5a6c"
-        page = client.get_block(url)
+#         url = "https://www.notion.so/cecf1c9de960437c80f4f3b9940a5a6c"
+#         page = client.get_block(url)
 
-        print("titulo antiguo" + page.title)
+#         print("titulo antiguo" + page.title)
 
-        page.title += " (Cambiado)"
-        bloquecodigo = page.children.add_new(CodeBlock, title="hooollaaa")
-        response = make_response()
+#         page.title += " (Cambiado)"
+#         bloquecodigo = page.children.add_new(CodeBlock, title="hooollaaa")
+#         response = make_response()
 
-        if bloquecodigo.id != None:
-            response = jsonify({"resultado": "ok"})
-        else:
-            response = jsonify({"resultado": "error"})
+#         if bloquecodigo.id != None:
+#             response = jsonify({"resultado": "ok"})
+#         else:
+#             response = jsonify({"resultado": "error"})
 
-        response.headers.add("Access-Control-Allow-Origin", "*")
-        return response
+#         response.headers.add("Access-Control-Allow-Origin", "*")
+#         return response
 
-    return redirect(url_for("home"))
+#     return redirect(url_for("home"))
